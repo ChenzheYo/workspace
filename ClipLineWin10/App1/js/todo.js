7ab7a59388ad513c5d1e9e5bea013cdcc5751ef0
chenzheYo <youshintetu@gmail.com>
2017-03-08 10:12:05 +0900

    init
    cliplineDemo

---
+++ ClipLineWin10/App1/js/todo.js
@@ -0,0 +1,43 @@
+﻿//// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+//// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+//// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+//// PARTICULAR PURPOSE.
+////
+//// Copyright (c) Microsoft Corporation. All rights reserved
+"use strict";
+
+/// .Net処理を呼び出し、カメラを起動する関数
+/// 画面起動時に呼ばれる（body onload）
+function captureVideo() {
+    window.external.notify("captureVideo");
+}
+
+function pickVideo1() {
+    if (window.OURBRIDGEOBJ) {
+        var sss = window.OURBRIDGEOBJ.pickFile();
+        window.location.href = sss; 
+    }
+
+}
+
+/// .Net処理を呼び出し、ファイルを選択する関数
+/// 画面起動時に呼ばれる（body onload）
+function pickVideo() {
+    window.external.notify("pickVideo");
+}
+
+/// .Net処理からの戻り値を受け取る関数
+function setVideo(videoPath) {
+    //var videoBlobUrl = URL.createObjectURL(file, { oneTimeOnly: true });
+    //document.getElementById("capturedVideo").src = "file:///" + videoPath;
+    document.getElementById("capturedVideo").src = "http://localhost:8080/ClipLine/oceans.mp4";
+    document.getElementById("resetButton").style.visibility = "visible";
+    document.getElementById("debugmsg").textContent = document.getElementById("capturedVideo").src;
+}
+
+/// 画面に表示している動画をクリアする
+function reset() {
+    document.getElementById("capturedVideo").src = "";
+    document.getElementById("resetButton").style.visibility = "hidden";
+}
+
---
+++ ClipLineWin10/RuntimeComponent1/ClipLineBridge.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Metadata;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Popups;
+
+namespace ClipLineBridge
+{
+    [AllowForWeb]
+    public sealed class ClipLineBridge
+    {
+        public async void pickFile()
+        {
+            //CameraCaptureUI dialog = new CameraCaptureUI();
+            //Size aspectRatio = new Size(16, 9);
+            //dialog.PhotoSettings.CroppedAspectRatio = aspectRatio;
+            //StorageFile file = await dialog.CaptureFileAsync(CameraCaptureUIMode.Video);
+            FileOpenPicker pick = new FileOpenPicker();
+            pick.FileTypeFilter.Add(".mp4");
+            StorageFile file = await pick.PickSingleFileAsync();
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            StorageFile file2 = await file.CopyAsync(folder);
+
+            await Task.Run( () =>
+            {
+                File.Copy(file.Path, "C:\\Program Files (x86)\\Apache Software Foundation\\Tomcat 9.0\\webapps\\ROOT\\ClipLine\\a.mp4");
+            });
+        }
+    }
+}
